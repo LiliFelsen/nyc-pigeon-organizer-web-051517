935b24fcaaffcff938d9b8d11ffa2c593c119791
Aurelie Felsen <aurelie.felsen@gmail.com>
2017-05-08 21:33:46 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,13 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  organized_hash = {}
+  data.each do |property, property_details|
+    property_details.each do |attribute, attribute_details|
+      attribute_details.each do |pigeon_name|
+        organized_hash[pigeon_name] ||= {}
+        organized_hash[pigeon_name][property] ||= []
+        organized_hash[pigeon_name][property] << attribute.to_s
+      end
+    end
+  end
+  organized_hash
 end
